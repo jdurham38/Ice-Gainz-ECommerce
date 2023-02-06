6f0f43c3c871721da0c7f2d99314e31e4fe66424
Christina Aragon <62121469+csaragon1941@users.noreply.github.com>
2023-02-05 19:25:00 -0500

    my first commit...yuh

---
+++ constants/data.js
@@ -0,0 +1,117 @@
+const userData = {
+  githubUsername: "csaragon1941",
+  name: "Christina Aragon",
+  designation: "Certified SAFe® 5 Product Owner/ Product Manager",
+  avatarUrl: "/C.Aragon-Avatar.jpg",
+  email: "aragonchristina2@gmail.com",
+  phone: "+1 305-282-4155",
+  address: "Naples, Florida, U.S.A",
+  projects: [
+    {
+      title: "Discord Music Bot",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "App for Undiclosed Internship",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a recently graduated Software Engineer who is passionate about creating innovative projects for tommorow",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Ice Gainz LLC.",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "mroads",
+      year: "2021",
+      companyLink: "https://mroads.com",
+      desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
+    },
+    {
+      title: "Founder and Developer",
+      company: "TailwindMasterKit",
+      year: "2021",
+      companyLink: "https://tailwindmasterkit.com",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Founder and Developer",
+      company: "Algochurn",
+      year: "2021",
+      companyLink: "https://algochurn.com",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Internship",
+      company: "Undisclosed",
+      year: "2023",
+      companyLink: "",
+      desc: "Helped in the development of an important goverment program that provides users with a rapid way to obtain their benefits.",
+    },
+    {
+      title: " College Graduation",
+      company: "Florida Gulf Coast University",
+      year: "2023",
+      companyLink: "https://www.fgcu.edu/",
+      desc: "Apart from being involved, I worked hard in acheiving my undergraduate in Software Engineering!",
+    },
+    {
+      title: "High School",
+      company: "Westminster Christian School",
+      year: "2018",
+      companyLink: "https://www.wcsmiami.org/",
+      desc: "Straight out of my hometown Miami, FL.I took courses pertaining to Engineering that gave me experience with AutoCAD.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/christina.aragonn",
+    twitter: "https://twitter.com/mannupaaji",
+    linkedin: "https://www.linkedin.com/in/christina-aragon11/",
+    github: "https://github.com/csaragon1941",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
