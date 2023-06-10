2bab9a6b5532ac0986281723a680918038f946ea
Joshua Durham <jdurham9725@eagle.fgcu.edu>
2023-06-10 13:43:18 -0400

    update game page

--- components/Game.js
+++ components/Game.js
@@ -1,34 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { DataStore } from '@aws-amplify/datastore';
-import { Products } from './models';
 
 
-function ProductComponent() {
-  const [product, setProduct] = useState(null);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const products = await DataStore.query(Products);
-      if (products.length > 0) {
-        setProduct(products[0]);
-      }
-    };
 
-    fetchProduct();
-  }, []);
+  export default function Game() {
 
-  return (
-    <div>
-      {product ? (
-        <div>
-          <h2>{product.products}</h2>
-          <p>Price: {product.price}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
-}
-
-export default ProductComponent;
+    return (
+      <div>
+          <h1>Under Maitenance</h1>
+      </div>
+  
+    );
+  }
+  
