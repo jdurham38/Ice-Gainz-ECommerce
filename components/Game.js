d2ca58b193d991e61fd46a615959264a379081b6
jdurham38 <60802210+jdurham38@users.noreply.github.com>
2023-06-10 14:10:16 -0400

    Add files via upload

---
+++ components/Game.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+
+
+
+  export default function Game() {
+
+    return (
+      <div>
+          <h1>Under Maitenance</h1>
+      </div>
+  
+    );
+  }
+  
